8f3a5528cc1a62e193db76facedd8daff78b5c71
djasiqi <jasiqi.drin@gmail.com>
2026-01-13 00:53:05 +0100

    fix(db): add migration merge for AVS number branches
    - Merge bf7baf7a0f6f and 2d6acf22f1f6
    - Resolves duplicate AVS number migrations
    - Fixes 'column user.password_expires_at does not exist' error in production

---
+++ backend/migrations/versions/ab24477a2d97_merge_avs_number_branches.py
@@ -0,0 +1,23 @@
+"""Merge AVS number branches
+
+Revision ID: ab24477a2d97
+Revises: ('bf7baf7a0f6f', '2d6acf22f1f6')
+Create Date: 2026-01-12 23:51:12.987847
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = "ab24477a2d97"
+down_revision = ('bf7baf7a0f6f', '2d6acf22f1f6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
